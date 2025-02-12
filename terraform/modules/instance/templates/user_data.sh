d901e147077c7157e20f387ad53a29e001626ed6
Alfie Mellor <inbox@alfiemellor.com>
2025-02-12 21:59:54 +0000

    Add argocd bootstrap configuration

--- terraform/modules/instance/templates/user_data.sh
+++ terraform/modules/instance/templates/user_data.sh
@@ -15,6 +15,31 @@ if [[ "$HOSTNAME" =~ "oci-k3s-server" ]]; then
     SERVER_PRIVATE_IP=$(hostname -I | awk '{print $1}')
     echo "Starting k3s server, node IP: $SERVER_PRIVATE_IP, and external IP: $SERVER_PUBLIC_IP"
     curl -sfL https://get.k3s.io | K3S_TOKEN="${token}" sh -s - server --node-ip=$SERVER_PRIVATE_IP --node-external-ip=$SERVER_PUBLIC_IP --tls-san=$SERVER_PRIVATE_IP --tls-san=$SERVER_PUBLIC_IP --cluster-init --disable traefik
+    
+    # Wait for Kubernetes API to be available
+    while ! kubectl get nodes; do sleep 5; done
+
+    # Define the total number of nodes in the cluster (master + worker nodes)
+    EXPECTED_NODES=4  # TODO: Pull this number as a var from Terraform based on the number of installed nodes
+    READY_NODES=0
+
+    while [ $READY_NODES -lt $EXPECTED_NODES ]; do
+        READY_NODES=$(kubectl get nodes --no-headers | grep -c 'Ready')
+        echo "Waiting for nodes to be ready... ($READY_NODES/$EXPECTED_NODES)"
+        sleep 5
+    done
+
+    # Install ArgoCD
+    kubectl create namespace argocd
+    kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+    while [[ $(kubectl get pods -n argocd --field-selector=status.phase!=Running | wc -l) -gt 1 ]]; do
+        echo "Waiting for ArgoCD to be fully ready..."
+        sleep 10
+    done
+
+    # Bootstrap ArgoCD applications
+    kubectl apply -f https://raw.githubusercontent.com/alfiemellor/homelab/main/apps/argocd/bootstrap.yaml    
 else
     echo "Starting k3s agent, server IP: ${k3s_server_private_ip}"
     until (curl -sfL https://get.k3s.io | K3S_TOKEN="${token}" K3S_URL="https://${k3s_server_private_ip}:6443" sh -s); do
